93ae4b501c10cc5772d08fded95b4dfd50c7dc06
sudhir rangu <sudhir@Mac-904.lan>
2025-12-15 07:27:49 -0500

    Add simple test page to verify global header

---
+++ app/simple/page.tsx
@@ -0,0 +1,9 @@
+export default function SimplePage() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>Simple Test Page</h1>
+      <p>This page should show the global header from root layout.</p>
+      <p>If you can see the header here, then the global layout is working.</p>
+    </div>
+  )
+}
